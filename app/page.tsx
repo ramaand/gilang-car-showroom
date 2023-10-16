e626e80373fcf82b9d6acdeecc17cf3c2854e092
Gilang Ramadhan <gilangramadhanii191@gmail.com>
2023-10-16 15:33:51 +0700

    feat(navbar): assign navbar to home (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/Section/About'
 import Cars from '@/components/Section/Cars'
@@ -5,6 +6,7 @@ import Cars from '@/components/Section/Cars'
 export default function Home() {
   return (
     <main className="max-w-[1920px] bg-white mx-auto relative overflow-hidden">
+      <Header />
       <Hero />
       <Cars />
       <About />
