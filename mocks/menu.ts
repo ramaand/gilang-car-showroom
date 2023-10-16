4ea3750a3500f2b52ef7eca642512a9c08c0b8db
Gilang Ramadhan <gilangramadhanii191@gmail.com>
2023-10-16 15:27:31 +0700

    chore(mock): add menu data

---
+++ mocks/menu.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    to: 'home',
+    label: 'Home',
+  },
+  {
+    to: 'cars',
+    label: 'Cars',
+  },
+  {
+    to: 'about',
+    label: 'About',
+  },
+  {
+    to: 'contact',
+    label: 'Contact',
+  },
+]
