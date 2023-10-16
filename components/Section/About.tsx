ce04f6fb3ec47191b3c281c2bb3433ca18dd3897
Gilang Ramadhan <gilangramadhanii191@gmail.com>
2023-10-16 15:23:36 +0700

    feat(section): add sections

---
+++ components/Section/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="h-screen flex items-center bg-green-200" id="about">
+      About
+    </section>
+  )
+}
+
+export default About
---
+++ components/Section/Cars.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cars = () => {
+  return (
+    <section className="h-screen flex items-center bg-gray-200" id='cars'>Cars</section>
+  )
+}
+
+export default Cars
