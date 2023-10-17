19d812f11eacf65137c73dd385ab5c79c48c598c
Gilang Ramadhan <gilangramadhanii191@gmail.com>
2023-10-17 12:49:59 +0700

    feat(car): update car section

--- components/Section/Cars.tsx
+++ components/Section/Cars.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
+'use client'
+import React, { useMemo } from 'react'
+
+import { CarProps } from '@/types'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+import { HEADERS, RAPID_API_URL } from '@/constant'
+
+import CarCard from '@/components/Car/CarCard'
+import CarEmpty from '@/components/Car/CarEmpty'
+import RenderIf from '@/components/RenderIf'
+import CarSkeleton from '@/components/Skeletons/CarSkeleton'
 
 const Cars = () => {
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ['cars'],
+    queryFn: async () => {
+      const { data } = await axios.get(RAPID_API_URL, {
+        headers: HEADERS,
+        params: { model: 'corolla' },
+      })
+      return data
+    },
+  })
+
+  const emptyDataArray = Array.from({ length: 8 }, () => '')
+
+  const isDataEmpty = useMemo(
+    () => Array.isArray(data) && !data.length && !isLoading && !isError,
+    [data, isLoading, isError]
+  )
+
   return (
-    <section className="h-screen flex items-center bg-gray-200" id='cars'>Cars</section>
+    <section
+      className="min-h-screen py-20 flex items-center bg-gray-100"
+      id="cars"
+    >
+      <div className="container mx-auto">
+        <RenderIf isTrue={isLoading}>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {emptyDataArray.map((item, i) => (
+              <CarSkeleton key={'skeleton-car-' + i} />
+            ))}
+          </div>
+        </RenderIf>
+        {isDataEmpty ? (
+          <CarEmpty />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {data?.map((car: CarProps, i: number) => (
+              <CarCard key={car.city_mpg + car.model + i} car={car} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
