ec9e1e579178657b0293ee3ac0c38971a2c6b010
Gilang Ramadhan <gilangramadhanii191@gmail.com>
2023-10-17 22:41:52 +0700

    feat(provider): add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import TanStackProvider from '@/providers/TanStackProvider'
+import ToasterProvider from '@/providers/ToastProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TanStackProvider>{children}</TanStackProvider>
+        <TanStackProvider>
+          <ToasterProvider />
+          {children}
+        </TanStackProvider>
       </body>
     </html>
   )
